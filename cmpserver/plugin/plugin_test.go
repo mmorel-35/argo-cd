28bed38264fc9eacd7c4046b6ebdaebfadfde259
Matthieu MOREL <matthieu.morel35@gmail.com>
2024-06-17 17:59:59 +0200

    Update plugin_test.go
    Signed-off-by: Matthieu MOREL <matthieu.morel35@gmail.com>

--- cmpserver/plugin/plugin_test.go
+++ cmpserver/plugin/plugin_test.go
@@ -133,7 +133,7 @@ func TestMatchRepository(t *testing.T) {
 		_, _, err := f.service.matchRepository(context.Background(), f.path, f.env, ".")
 
 		// then
-		assert.ErrorContains(t, err, "syntax error")
+		require.ErrorContains(t, err, "syntax error")
 	})
 	t.Run("will match plugin by glob", func(t *testing.T) {
 		// given
@@ -182,7 +182,7 @@ func TestMatchRepository(t *testing.T) {
 		_, _, err := f.service.matchRepository(context.Background(), f.path, f.env, ".")
 
 		// then
-		assert.ErrorContains(t, err, "error finding glob match for pattern")
+		require.ErrorContains(t, err, "error finding glob match for pattern")
 	})
 	t.Run("will match plugin by command when returns any output", func(t *testing.T) {
 		// given
@@ -323,7 +323,7 @@ func TestGenerateManifest(t *testing.T) {
 		service.WithGenerateCommand(Command{Command: []string{"bad-command"}})
 
 		res, err := service.generateManifest(context.Background(), "testdata/kustomize", nil)
-		assert.ErrorContains(t, err, "executable file not found")
+		require.ErrorContains(t, err, "executable file not found")
 		assert.Nil(t, res.Manifests)
 	})
 	t.Run("bad yaml output", func(t *testing.T) {
@@ -332,7 +332,7 @@ func TestGenerateManifest(t *testing.T) {
 		service.WithGenerateCommand(Command{Command: []string{"echo", "invalid yaml: }"}})
 
 		res, err := service.generateManifest(context.Background(), "testdata/kustomize", nil)
-		assert.ErrorContains(t, err, "failed to unmarshal manifest")
+		require.ErrorContains(t, err, "failed to unmarshal manifest")
 		assert.Nil(t, res.Manifests)
 	})
 }
@@ -345,7 +345,7 @@ func TestGenerateManifest_deadline_exceeded(t *testing.T) {
 	expiredCtx, cancel := context.WithTimeout(context.Background(), time.Second*0)
 	defer cancel()
 	_, err = service.generateManifest(expiredCtx, "", nil)
-	assert.ErrorContains(t, err, "context deadline exceeded")
+	require.ErrorContains(t, err, "context deadline exceeded")
 }
 
 // TestRunCommandContextTimeout makes sure the command dies at timeout rather than sleeping past the timeout.
@@ -366,7 +366,7 @@ func TestRunCommandContextTimeout(t *testing.T) {
 
 func TestRunCommandEmptyCommand(t *testing.T) {
 	_, err := runCommand(context.Background(), Command{}, "", nil)
-	assert.ErrorContains(t, err, "Command is empty")
+	require.ErrorContains(t, err, "Command is empty")
 }
 
 // TestRunCommandContextTimeoutWithCleanup makes sure that the process is given enough time to cleanup before sending SIGKILL.
@@ -472,7 +472,7 @@ func Test_getTempDirMustCleanup(t *testing.T) {
 	err := os.Chmod(tempDir, 0o000)
 	require.NoError(t, err)
 	_, _, err = getTempDirMustCleanup(path.Join(tempDir, "test"))
-	assert.ErrorContains(t, err, "error creating temp dir")
+	require.ErrorContains(t, err, "error creating temp dir")
 
 	err = os.Chmod(tempDir, 0o700)
 	require.NoError(t, err)
@@ -500,7 +500,7 @@ func TestService_Init(t *testing.T) {
 	require.NoError(t, err)
 	s := NewService(CMPServerInitConstants{PluginConfig: PluginConfig{}})
 	err = s.Init(workDir)
-	assert.ErrorContains(t, err, "error removing workdir", "Init must throw an error if it can't remove the work directory")
+	require.ErrorContains(t, err, "error removing workdir", "Init must throw an error if it can't remove the work directory")
 
 	// Make the base directory writable so Init's cleanup succeeds.
 	err = os.Chmod(tempDir, 0o700)
